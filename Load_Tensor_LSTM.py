35c8e9d689366dc3497b10e05062ae7bc2f18a15
BuildTools <unconfigured@null.spigotmc.org>
2019-04-17 16:45:45 -0400

    Create Load_Tensor_LSTM.py

---
+++ Load_Tensor_LSTM.py
@@ -0,0 +1,26 @@
+import torch
+import glob
+
+
+print('Loading data')
+data_train = []
+target_train = []
+data_valid = []
+target_valid = []
+data_test = []
+target_test = []
+compteur = 0
+for im_path in glob.glob("LSTM_Input/LSTM_Target*.pt"):
+    dataStr = im_path[im_path.find('Target_') +7 :-1] + 't'
+    im_pathData = 'LSTM_Input/LSTM_Data_' + dataStr
+    if compteur < 100 :
+        target_train.append(torch.load(im_path))
+        data_train.append(torch.load(im_pathData))
+    if compteur >= 100 and compteur < 122:
+        target_valid.append(torch.load(im_path))
+        data_valid.append(torch.load(im_pathData))
+    if compteur >= 122:
+        target_test.append(torch.load(im_path))
+        data_test.append(torch.load(im_pathData))
+    compteur += 1
+print('Done loading data')
