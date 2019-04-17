bb31b7a5d27f5eb54767e42e26eaf7e5a1b2964d
BuildTools <unconfigured@null.spigotmc.org>
2019-04-17 12:40:28 -0400

    Update model.py

--- model.py
+++ model.py
@@ -3,17 +3,19 @@ import torch
 from torch.autograd import Variable
 import numpy as np
 import torch.nn.functional as F
-#import torchvision.transforms as transforms
+import torchvision.transforms as transforms
 from torch import nn
 from torch import optim
 import imageio
 import glob
 import time
+from random import shuffle
+
 
 # test = torch.from_numpy(np.array(imageio.imread('C:/Users/DenisCorbin/Desktop/CIL-1/Data/0001.png'))).view(1,1,480,640).double()
 # conv = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1).double()
 # result = conv(test)
-batch_size = 1
+batch_size = 5
 
 
 # %%
@@ -49,7 +51,7 @@ class CNNEncoderDecoder(nn.Module):
 
             nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1).double(),
             nn.BatchNorm2d(1).double(),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
 
         )
         self.decoder = nn.Sequential(
@@ -84,14 +86,14 @@ class CNNEncoderDecoder(nn.Module):
 
             nn.Conv2d(in_channels=1, out_channels=1, kernel_size=3, stride=1, padding=1).double(),
             nn.BatchNorm2d(1).double(),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=True)
 
         )
 
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
-        return x / x.max()
+        return x/x.max()
 
 
 # %%
@@ -125,46 +127,44 @@ for im_path in glob.glob("C:/Users/DenisCorbin/Desktop/CIL1/Annotation/Output0/*
     compteur += 1
 print('Done loading data')
 
+#train_loader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, shuffle=True)
+
+#torch.cat(torch.stack(data_train),torch.stack(target_train))
+#shuffle(target_train)
+#target_train = torch.stack(target_train)
+
 
 # %%
 
 def LLE(output, maxY_t,max_X_t):    
-    partial_sumx = 0
-    for x in range(output.size(3)):
-        partial_sumx += (output[0, 0, :, x].sum()) * x
-    x_pred = partial_sumx / output.sum()
-    
-    partial_sumy = 0
-    for y in range(output.size(2)):
-        partial_sumy += (output[0, 0, y, :].sum()) * y
-    y_pred = partial_sumy / output.sum()
-
-    LLE = torch.sqrt((torch.from_numpy(max_X_t).double() - x_pred).pow(2) + (torch.from_numpy(maxY_t).double() - y_pred).pow(2))
-    return LLE, x_pred, y_pred
-
-def LLE2(output, maxY_t,max_X_t):    
     pixel_sum = output.sum()
     
     #Calcul du x predit
     v_x = torch.arange(output.size(3)).double() #Vecteur allant de 0 a 639
     partial_sumx = torch.mv(output.squeeze(),v_x).sum() #Somme ponderee selon x
+#    for x in range(output.size(3)):
+#        partial_sumx_long += (output[0, 0, :, x].sum()) * x
     x_pred = partial_sumx / pixel_sum
     
     output_T = torch.transpose(output,2,3) #Transposition de la matrice
     v_y = torch.arange(output.size(2)).double()
     partial_sumy = torch.mv(output_T.squeeze(),v_y).sum()
+#    partial_sumy_long = 0
+#    for y in range(output.size(2)):
+#        partial_sumy_long += (output[0, 0, y, :].sum()) * y
     y_pred = partial_sumy / pixel_sum
 
     LLE = torch.sqrt((torch.from_numpy(max_X_t).double() - x_pred).pow(2) + (torch.from_numpy(maxY_t).double() - y_pred).pow(2))
-    return LLE, x_pred, y_pred
-
+    print(LLE.item(), max_X_t, maxY_t, x_pred.item(), y_pred.item())
+    return LLE
 
 
 def train(model, train_loader, optimizer):
     model.train()
     train_loss = 0
+    loss = 0
     dataSize = (len(train_loader))
-    for batch_idx in range(dataSize):
+    for batch_idx in range(int(dataSize)):
         # data, target = Variable(data, volatile=True).cuda(), Variable(target).cuda() # if you have access to a gpu
         data, target = Variable(train_loader[batch_idx]), Variable(target_train[batch_idx])
         optimizer.zero_grad()
@@ -173,13 +173,15 @@ def train(model, train_loader, optimizer):
         #target_np = target.detach().numpy().squeeze()
         #position = np.where(target_np == target_np.max())
         maxY_t, max_X_t = np.where(target.squeeze() == target.max())
-        loss, x_pred, y_pred = LLE2(output, maxY_t,max_X_t)#+F.mse_loss(output, target)
-#        loss = F.l1_loss(output,target) 
-#        loss = LLE_Denis(output, target)
-        print(loss.item() , max_X_t, maxY_t, x_pred.detach().numpy().squeeze(), y_pred.detach().numpy().squeeze())
+        loss += F.mse_loss(output, target) + LLE(output, maxY_t,max_X_t)
         train_loss += loss.item()
-        loss.backward()
-        optimizer.step()
+        #t = time.time()
+        if batch_idx%batch_size==0:
+            loss /= batch_size
+            loss.backward()
+            #print(time.time() - t)
+            optimizer.step()
+            loss = 0
     train_loss /= (dataSize / batch_size)
     # print('\n' + "train" + ' set: Average loss: {:.4f}\n'.format(train_loss))
     return model, train_loss
@@ -195,18 +197,16 @@ def valid(model, valid_loader):
         data, target = Variable(valid_loader[batch_idx], volatile=True), Variable(target_valid[batch_idx])
         output = model(data)
         maxY_t, max_X_t = np.where(target.squeeze() == target.max())
-        loss, x_pred, y_pred = LLE2(output, maxY_t,max_X_t)#+F.mse_loss(output, target)
-        valid_loss += loss  # sum up batch loss
-        print(loss.item())
-        if loss.item() < 15:
+        loss = F.mse_loss(output, target) + LLE(output, maxY_t,max_X_t)
+        valid_loss += loss.item()  # sum up batch loss
+        if loss.item() < 33:
             correct += 1;
-#        correct += 1
         #pred = output.data.max(1, keepdim=True)[1]  # get the index of the max log-probability
         #correct += pred.eq(target.data.view_as(pred).long()).cpu().sum() / 480 / 640
 
     valid_loss /= dataSize
     print('\n' + "valid" + ' set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        float(valid_loss.detach().numpy().squeeze()), correct, dataSize,
+        valid_loss, correct, dataSize,
         100. * correct / dataSize))
     return 100. * correct / dataSize, valid_loss
 
@@ -241,7 +241,7 @@ def experiment(model, epochs=10, lr=0.001):
         precision, valid_loss = valid(model, data_valid)
         valid_losses.append(valid_loss)
         valid_precision.append(precision)
-        if precision >= best_precision:
+        if precision > best_precision:
             best_precision = precision
             best_model = model
         best_model = model
@@ -266,13 +266,18 @@ def experiment(model, epochs=10, lr=0.001):
 # %%
 best_precision = 0
 for model in [CNNEncoderDecoder()]:  # add your models in the list
-#    model.cuda()  # if you have access to a gpu
-    model, precision = experiment(model, epochs=10, lr=0.001)
+    #    model.cuda()  # if you have access to a gpu
+    model, precision = experiment(model, epochs=20, lr=0.01)
     if precision > best_precision:
         best_precision = precision
         best_model = model
 
-valeurs = best_model(data_train[0])
+valeurs = data_train[100]
+image = valeurs.detach().numpy().squeeze()
+valeurs = best_model(valeurs)
 image = valeurs.detach().numpy().squeeze()
 
 # test(best_model, test_loader)
+
+valeurs = target_valid[0]
+image = valeurs.detach().numpy().squeeze()
